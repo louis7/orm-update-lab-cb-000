21a74a46b489e8e5c68b920e5e3ccce12ff2d7a3
louis baptiste <louisvis@ymail.com>
2018-10-25 00:46:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ end
 
 def self.find_by_name(name)
   stored_name=name
-  sql = "SELECT tudents WHERE name = ?"
+  sql = "SELECT students WHERE name = ?"
   DB[:conn].execute(sql, stored_name)
 
 end
